c6002f1c53074beab4057b45b58b2a0338022d83
Troy Denton <trdenton@gmail.com>
2017-03-13 21:01:02 -0500

    now running commands out of configuration file

--- udptrigger.py
+++ udptrigger.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python
-import ConfigParser, os, sys, socket
-import subprocess
+import ConfigParser, os, sys, socket, subprocess
 
 config = ConfigParser.ConfigParser()
 config.read(sys.argv[1])
-
 
 udp_ip = config.get("Network","ip")
 udp_port = config.getint("Network","port")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 sock.bind( (udp_ip,udp_port) )
 
 cmds = config.items("Commands")
 
 while True:
   data,addr = sock.recvfrom(128)
-  print "received ",data
 
   for cmd in cmds:
     if cmd[0] in data:
